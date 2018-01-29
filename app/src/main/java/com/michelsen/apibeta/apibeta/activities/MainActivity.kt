5d19494d56ac61f390d2fe376d9e00f52a19adbb
Ole-Martin Michelsen <om.michelsen@gmail.com>
2018-01-29 11:56:14 +0100

    Example of using accounts

--- app/src/main/java/com/michelsen/apibeta/apibeta/activities/MainActivity.kt
+++ app/src/main/java/com/michelsen/apibeta/apibeta/activities/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.michelsen.apibeta.apibeta.activities
 
+import android.accounts.Account
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.michelsen.apibeta.apibeta.R
 import com.michelsen.apibeta.apibeta.helpers.Data
+import com.michelsen.apibeta.apibeta.models.Accounts
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var mData: Data? = null
@@ -23,7 +26,16 @@ class MainActivity : AppCompatActivity() {
         mData?.getAccounts();
     }
 
+    /*
+     * Sample for using accounts
+     */
     fun buttonClick2(view: View) {
+        var str = ""
+        var list = mData?.accounts?.accounts
+        for (account in list!!) {
+            str += account.name + ", "
+        }
+        text_view.text = str
     }
 
 
